339290bc1cddcc6de3fda150fcffd874a00009be
Marcin Swierczek <mswierczek90@gmail.com>
2023-10-17 21:44:47 +0200

    Fix backend/clieny.proto

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/session/client.proto")?;
     tonic_build::compile_protos("proto/session/token_verifier.proto")?;
+    tonic_build::compile_protos("proto/backend/client.proto")?;
     Ok(())
 }
